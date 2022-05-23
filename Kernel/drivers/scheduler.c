30ec272734d84ebaba85e5abf75650a0c7c4159d
IanSzejer <ianszejer@gmail.com>
2022-05-23 18:09:12 -0300

    Create procces avanzado NO ESTA TERMINADO

--- Kernel/drivers/scheduler.c
+++ Kernel/drivers/scheduler.c
@@ -1,7 +1,39 @@
 #include "stdint.h"
 #include "../include/lib.h"
+#include "../include/context.h"
+#include "../include/memoryDriverPropio.h"
+
 #define MAX_NAME 100
 #define FIRST_PID 1
+#define PROCESS_SIZE 10000      //El stack del proceso sera de 10000 bits
+#define BASE_PRIORITY 1
+typedef struct {
+	//Registers restore context
+	uint64_t rax;
+	uint64_t rbx;
+	uint64_t rcx;
+	uint64_t rdx;
+	uint64_t rbp;
+	uint64_t rdi;
+	uint64_t rsi;
+	uint64_t r8;
+	uint64_t r9;
+	uint64_t r10;
+	uint64_t r11;
+	uint64_t r12;
+	uint64_t r13;
+	uint64_t r14;
+	uint64_t r15;
+
+	//iretq hook
+	uint64_t rip;
+	uint64_t cs;
+	uint64_t rflags;
+	uint64_t rsp;
+	uint64_t ss;
+	uint64_t base;
+} StackFrame_t;
+
 
 typedef enum {
     FOREGROUND,
@@ -50,6 +82,8 @@ static ProcessNode* currentProcess;
 static ProcessNode* dummyProcess;
 static uint64_t pidCounter = 1;
 
+void* createContext(void* stack,uint16_t* arguments,void* (*funcion)(void*),int argc);
+
 // Cuando cree un proceso nuevo voy a querer q tenga otro pid
 static uint64_t getNewPid() {
     return pidCounter++;
@@ -65,12 +99,21 @@ void initializeScheduler(char* argv[]) {
     //     //corro memory manager
     // }
 }
+//Como argumento recibe un puntero a funcion, como es un proceso no se que parametros recibe
+//Por ahora digo que devuelve void*, por decreto recibe hasta 3 argumentos de tamaño uint64_t
+void createProcess(void* (*funcion)(void*),void* argv,int argc) {
+    uint64_t* arguments= mallocFun(3*sizeof(uint16_t));
+    memcpy(arguments,argv,3);
+    void* stack = mallocFun(PROCESS_SIZE);
+    void* stackTopPtr =createContext(stack,arguments,funcion,argc);
 
-void createProcess() {
-    // espacio para contexto
-    ProcessNode* newProcess = malloc(sizeof(ProcessNode));
-    // posicion de la primera instruccion
-    // poner en la lista de procesos
+    ProcessNode* newProcess = mallocFun(sizeof(ProcessNode));
+    newProcess->priority=BASE_PRIORITY;
+    newProcess->pcb.pid=getNewPid();
+    newProcess->pcb.fdIn=0;
+    newProcess->pcb.fdOut=1;
+    newProcess->pcb.state=stackTopPtr;      //Aca se tiene que empezar a popear, no estoy seguro
+    //faltan cosas del pcb pero queria verlo con ustedes
     addProcess(newProcess);
     // asignar prioridad
 }
@@ -269,4 +312,32 @@ uint64_t changeState(uint64_t pid, states newState) {
 
     processNode->pcb.state = newState;
     return 0;
+}
+
+
+void* createContext(void* stack,uint16_t* arguments,void* (*funcion)(void*),int argc){
+    StackFrame_t* stackStruct=(StackFrame_t*)stack;
+    stackStruct->base=stack;
+    stackStruct->ss=0x0;
+    stackStruct->rsp=stackStruct->base;
+    stackStruct->rflags=0x202;
+    stackStruct->cs=0x8;
+    stackStruct->rip=funcion;
+    stackStruct->r15=0;
+    stackStruct->r14=0;
+    stackStruct->r13=0;
+    stackStruct->r12=0;
+    stackStruct->r11=0;
+    stackStruct->r10=0;
+    stackStruct->r9=0;
+    stackStruct->r8=0;
+    stackStruct->rsi=arguments;
+    stackStruct->rdi=argc;
+    stackStruct->rbp=stackStruct->base;
+    stackStruct->rdx=0;
+    stackStruct->rcx=0;
+    stackStruct->rbx=0;
+    stackStruct->rax=0;
+    return &stackStruct->rax;
+
 }
